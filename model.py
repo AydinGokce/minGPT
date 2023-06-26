b36f0187e8b10b48b3190b33e82cd934a5284017
aydin <aydin@saronic.com>
2023-06-25 20:50:06 -0500

    modified training and generate script, worksgit add .

--- model.py
+++ model.py
@@ -2,9 +2,22 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 torch.manual_seed(1337)
+import time
+import os
+
+def clear_console():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+def last_20_lines(text):
+    text = text.split('\n')
+    text = text[-20:]
+    return '\n'.join(text)
 
 class SingleHeadAttention(nn.Module):
-    def __init__(self, embedding_size, head_size, context_length):
+    def __init__(self, embedding_size, head_size, context_length, dropout):
         super().__init__()
 
         self.embedding_size = embedding_size
@@ -12,6 +25,7 @@ class SingleHeadAttention(nn.Module):
         self.key = nn.Linear(embedding_size, head_size, bias=False)
         self.query = nn.Linear(embedding_size, head_size, bias = False)
         self.register_buffer('tril', torch.tril(torch.ones(context_length, context_length)))
+        self.dropout = nn.Dropout(dropout)
 
 
     def forward(self, X):
@@ -26,29 +40,32 @@ class SingleHeadAttention(nn.Module):
         # masking
         weights = weights.masked_fill(self.tril == 0, float("-inf")) # TODO: karpathy added [:T, :T] to tril, dunnot the point of that so omitting
         weights = F.softmax(weights, dim=1)
+        weights = self.dropout(weights)
 
         # readout
         return weights @ self.val(X) # (B, T, T) @ (B, T, head_size) -> (B, T, head_size)
     
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, n_heads, embedding_size, head_size, context_length, projection_factor = 4):
+    def __init__(self, n_heads, embedding_size, head_size, context_length, dropout, projection_factor = 4):
         super().__init__()
-        self.heads = nn.ModuleList([SingleHeadAttention(embedding_size, head_size * projection_factor, context_length) for _ in range(n_heads)])
+        self.heads = nn.ModuleList([SingleHeadAttention(embedding_size, head_size * projection_factor, context_length, dropout) for _ in range(n_heads)])
         self.proj = nn.Linear(embedding_size * projection_factor, embedding_size)
+        self.dropout = nn.Dropout(dropout)
     
     def forward(self, X):
         out = torch.concatenate([head(X) for head in self.heads], dim = -1)
-        return X + self.proj(out)
+        return self.dropout(X) + self.proj(out)
 
 
 class FeedForward(nn.Module):
-    def __init__(self, in_ch, out_ch, projection_factor = 4):
+    def __init__(self, in_ch, out_ch, dropout, projection_factor = 4):
         super().__init__()
         self.linear = nn.Sequential(
             nn.Linear(in_ch, out_ch * projection_factor),
             nn.ReLU(),
-            nn.Linear(out_ch * projection_factor, out_ch)
+            nn.Linear(out_ch * projection_factor, out_ch),
+            nn.Dropout(dropout)
         )
 
     def forward(self, X):
@@ -57,11 +74,14 @@ class FeedForward(nn.Module):
 
 class Block(nn.Module):
 
-    def __init__(self, n_heads, embedding_size, head_size, context_length):
+    def __init__(self, n_heads, embedding_size, head_size, context_length, dropout):
         super().__init__()
         self.block = nn.Sequential(
-            MultiHeadAttention(n_heads, embedding_size, head_size, context_length),
-            FeedForward(embedding_size, embedding_size)
+            nn.LayerNorm(embedding_size),
+            MultiHeadAttention(n_heads, embedding_size, head_size, context_length, dropout),
+            nn.LayerNorm(embedding_size),
+            FeedForward(embedding_size, embedding_size, dropout)
+        
         )
 
     def forward(self, X):
@@ -70,7 +90,7 @@ class Block(nn.Module):
 
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size, embedding_size, context_length, device, n_heads):
+    def __init__(self, vocab_size, embedding_size, context_length, device, n_heads, dropout):
         super().__init__()
         self.context_length = context_length
         self.embedding_size = embedding_size
@@ -82,14 +102,19 @@ class BigramLanguageModel(nn.Module):
         self.readout_head = nn.Linear(embedding_size, vocab_size)
     
         # multi-head attention and feed-forward
-        self.block = Block(n_heads, embedding_size, embedding_size // n_heads, context_length)
+        self.blocks = nn.Sequential(
+            Block(n_heads, embedding_size, embedding_size // n_heads, context_length, dropout),
+            Block(n_heads, embedding_size, embedding_size // n_heads, context_length, dropout),
+            Block(n_heads, embedding_size, embedding_size // n_heads, context_length, dropout),
+            nn.LayerNorm(embedding_size)
+        )
         
     def forward(self, X, y = None):
 
         token_embeddings = self.embedding_table(X) # (B, T, embedding_size)
         pos_embeddings = self.positional_embedding_table(torch.arange(self.context_length, device=self.device))
         x = token_embeddings + pos_embeddings
-        self.block(x)
+        x = self.blocks(x)
         logits = self.readout_head(x) # (B, T, embedding_size) @ (embedding_size, vocab_size) -> (B, T, vocab_size)
 
         loss = None
@@ -102,18 +127,21 @@ class BigramLanguageModel(nn.Module):
         return logits, loss # (32, 65), f32
 
 
-    def generate(self, prompt, max_num_steps):
+    def generate(self, prompt, decode=None, print_=True, frequency = 10):
         # prompt = (B, T)
+        with torch.no_grad():
+            output = prompt
+            while True:
+                prompt = output[:, -self.context_length:] # crop to context window
+                logits, _ = self(prompt)
+                logits = logits[:, -1, :] # B, C
+                logits = F.softmax(logits, dim = -1)
+                preds = torch.multinomial(logits, num_samples = 1)
+                if print_:
+                    clear_console() 
+                    print(last_20_lines(decode(output.cpu()[0].tolist())), end=" ")
+                    #time.sleep(1/frequency)
 
-        output = prompt
-            
-        for i  in range(max_num_steps):
-            prompt = output[:, -self.context_length:] # crop to context window
-            logits, _ = self(prompt)
-            logits = logits[:, -1, :] # B, C
-            logits = F.softmax(logits, dim = -1)
-            preds = torch.multinomial(logits, num_samples = 1)
+                output = torch.concatenate((output, preds), dim = 1)
 
-            output = torch.concatenate((output, preds), dim = 1)
-
-        return output
+            return output
--- train.py
+++ train.py
@@ -2,8 +2,10 @@ from encoder import Encoder
 from model import BigramLanguageModel
 import torch
 import os
+import wandb
 from alive_progress import alive_bar
 import logging
+from torch.optim.lr_scheduler import MultiStepLR
 
 def get_batch(dataset: list[int], batch_size: int, context_length: int) -> tuple[torch.Tensor, torch.Tensor]:
 
@@ -16,13 +18,18 @@ def get_batch(dataset: list[int], batch_size: int, context_length: int) -> tuple
 
 def main():
 
+    wandb.init(entity="aydingokce")
+
     ##### HYPERPARAMS #####
-    context_length = 8
-    batch_size = 128
+    context_length = 256
+    batch_size = 256
     train_test_split = 0.9
-    num_epochs = 10000
-    n_heads = 4
-    embedding_size = 32
+    num_epochs = 1
+    n_heads = 6
+    dropout = 0.2
+    embedding_size = 364
+    load_path = "model.pth"
+
     device = 'cuda' if torch.cuda.is_available else 'cpu'
     logging.info(f"using device {device}")
 
@@ -37,29 +44,42 @@ def main():
     test_enc = enc.encode(test_raw)
 
     ######## TRAIN ########
-    model = BigramLanguageModel(len(enc.vocabulary), embedding_size, context_length, device, n_heads).to(device)
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
 
-    with alive_bar(num_epochs) as bar:
-        for epoch_no in range(num_epochs):
+    if load_path is None:
+        model = BigramLanguageModel(len(enc.vocabulary), embedding_size, context_length, device, n_heads, dropout).to(device)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=5e-4)
+        scheduler = MultiStepLR(optimizer, milestones=[5000], gamma = 0.1)
 
-            optimizer.zero_grad()
-            X, y = get_batch(train_enc, batch_size, context_length)
-            X, y = X.to(device), y.to(device)
-            _, loss = model(X, y)
-            loss.backward()
-            optimizer.step()
+        with alive_bar(num_epochs) as bar:
+            for epoch_no in range(num_epochs):
 
-            bar()
+                optimizer.zero_grad()
+                X, y = get_batch(train_enc, batch_size, context_length)
+                X, y = X.to(device), y.to(device)
+                _, loss = model(X, y)
+                loss.backward()
+                item = loss.item()
+                optimizer.step()
+                bar.text = f"loss: {item}, lr: {scheduler.get_last_lr()}"
+                wandb.log({'training loss': item})
+                bar()
+
+                scheduler.step()
+    
+    else:
+        model.load_state_dict(torch.load(load_path))
 
     print(loss.item())
+    
+    torch.save(model.state_dict(), "model.pth")
 
     ######## TEST #########
     sample_x, sample_y = get_batch(test_enc, batch_size, context_length)
     input("training complete. press any key to see the output")
-    gen = model.generate(sample_x.to(device), 1000)[0]
-    print(enc.decode(gen.cpu().tolist()))
+    model.eval()
+    gen = model.generate(sample_x.to(device), enc.decode)
+    #print(enc.decode(gen.cpu().tolist()))
 
 
 
